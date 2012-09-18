aacbf876f56ed7186b253c28c67385211dd21838
kawakami-o3 <mikamihayato383@gmail.com>
2012-09-18 16:27:09 +0900

    整形

--- src/tree/OrderStatisticsTree.java
+++ src/tree/OrderStatisticsTree.java
@@ -24,18 +24,18 @@ public class OrderStatisticsTree extends AbstractBinaryTree<NodeOS> {
 	}
 
 	private NodeOS NIL;
-	
+
 	public OrderStatisticsTree() {
 		super();
 		NIL = new NodeOS(null);
 		setRoot(NIL);
-		
+
 		NIL.setParent(NIL);
 		NIL.setL(NIL);
 		NIL.setR(NIL);
 		NIL.setSize(0);
 	}
-	
+
 	@Override
 	public NodeOS getNil() {
 		return NIL;
@@ -72,7 +72,7 @@ public class OrderStatisticsTree extends AbstractBinaryTree<NodeOS> {
 		y.setR(x);
 		x.setParent(y);
 		y.setSize(x.getSize());
-		x.setSize(x.getL().getSize()+x.getR().getSize()+1);
+		x.setSize(x.getL().getSize() + x.getR().getSize() + 1);
 	}
 
 	public void rotateLeft(NodeOS x) {
@@ -100,7 +100,7 @@ public class OrderStatisticsTree extends AbstractBinaryTree<NodeOS> {
 		y.setL(x);
 		x.setParent(y);
 		y.setSize(x.getSize());
-		x.setSize(x.getL().getSize()+x.getR().getSize()+1);
+		x.setSize(x.getL().getSize() + x.getR().getSize() + 1);
 	}
 
 	public void insert(NodeOS z) {
@@ -108,7 +108,7 @@ public class OrderStatisticsTree extends AbstractBinaryTree<NodeOS> {
 		NodeOS x = getRoot();
 		while (x != getNil()) {
 			y = x;
-			y.setSize(y.getSize()+1);
+			y.setSize(y.getSize() + 1);
 			if (z.getKey() < x.getKey()) {
 				x = x.getL();
 			} else {
@@ -131,7 +131,7 @@ public class OrderStatisticsTree extends AbstractBinaryTree<NodeOS> {
 			}
 		}
 
-    sizeUp();
+		sizeUp();
 		insertFixUp(z);
 	}
 
@@ -177,7 +177,7 @@ public class OrderStatisticsTree extends AbstractBinaryTree<NodeOS> {
 
 	public NodeOS delete(NodeOS z) {
 		resizeForDelete(z);
-		
+
 		NodeOS y;
 		if (z.getL() == getNil() || z.getR() == getNil()) {
 			y = z;
@@ -212,20 +212,20 @@ public class OrderStatisticsTree extends AbstractBinaryTree<NodeOS> {
 			deleteFixUp(x);
 		}
 
-    sizeDown();
+		sizeDown();
 		return y;
 	}
 
 	public void resizeForDelete(NodeOS x) {
 		if (x != getNil()) {
-			x.setSize(x.getSize()-1);
+			x.setSize(x.getSize() - 1);
 			resizeForDelete(x.getParent());
 		}
 	}
 
 	public void deleteFixUp(NodeOS x) {
 		while (x != getRoot() && x.isBlack()) {
-//			System.out.println(x.getKey()+"-"+x.getParent().getKey());
+			// System.out.println(x.getKey()+"-"+x.getParent().getKey());
 			if (x == x.getParent().getL()) {
 				NodeOS w = x.getParent().getR();
 				if (w.getColor() == RED) {
@@ -286,51 +286,51 @@ public class OrderStatisticsTree extends AbstractBinaryTree<NodeOS> {
 		x.setColor(BLACK);
 	}
 
-    public NodeOS select(NodeOS x, int i) {
-        int r = x.getL().getSize() + 1;
-        if (i == r || x == getNil()) {
-            return x;
-        } else if (i<r) {
-            return select(x.getL(),i);
-        } else {
-            return select(x.getR(),i-r);
-        }
-    }
+	public NodeOS select(NodeOS x, int i) {
+		int r = x.getL().getSize() + 1;
+		if (i == r || x == getNil()) {
+			return x;
+		} else if (i < r) {
+			return select(x.getL(), i);
+		} else {
+			return select(x.getR(), i - r);
+		}
+	}
 
-    public NodeOS selectDesc(NodeOS x, int i) {
-        int r = x.getR().getSize() + 1;
-        if (i == r || x == getNil()) {
-            return x;
-        } else if (i<r) {
-            return selectDesc(x.getR(),i);
-        } else {
-            return selectDesc(x.getL(),i-r);
-        }
-    }
+	public NodeOS selectDesc(NodeOS x, int i) {
+		int r = x.getR().getSize() + 1;
+		if (i == r || x == getNil()) {
+			return x;
+		} else if (i < r) {
+			return selectDesc(x.getR(), i);
+		} else {
+			return selectDesc(x.getL(), i - r);
+		}
+	}
 
-    public int getRank(NodeOS x) {
-        int r = x.getL().getSize() + 1;
-        NodeOS y = x;
-        while (y!=getRoot()) {
-            if (y == y.getParent().getR()) {
-                r = r + y.getParent().getL().getSize() + 1;
-            }
-            y = y.getParent();
-        }
-        return r;
-    }
+	public int getRank(NodeOS x) {
+		int r = x.getL().getSize() + 1;
+		NodeOS y = x;
+		while (y != getRoot()) {
+			if (y == y.getParent().getR()) {
+				r = r + y.getParent().getL().getSize() + 1;
+			}
+			y = y.getParent();
+		}
+		return r;
+	}
 
-    public int getRankDesc(NodeOS x) {
-        int r = x.getR().getSize() + 1;
-        NodeOS y = x;
-        while (y!=getRoot()) {
-            if (y == y.getParent().getL()) {
-                r = r + y.getParent().getR().getSize() + 1;
-            }
-            y = y.getParent();
-        }
-        return r;
-    }
+	public int getRankDesc(NodeOS x) {
+		int r = x.getR().getSize() + 1;
+		NodeOS y = x;
+		while (y != getRoot()) {
+			if (y == y.getParent().getL()) {
+				r = r + y.getParent().getR().getSize() + 1;
+			}
+			y = y.getParent();
+		}
+		return r;
+	}
 
 	public void print() {
 		List<NodeOS> nodes = getListInorderWalk();
@@ -348,7 +348,8 @@ public class OrderStatisticsTree extends AbstractBinaryTree<NodeOS> {
 
 				if (i == depths.get(j)) {
 					NodeOS n = nodes.get(j);
-					System.out.printf("%5d[" + n.getColor().getShortName() + ",%3d]", n.getKey(),n.getSize());
+					System.out.printf("%5d[" + n.getColor().getShortName()
+							+ ",%3d]", n.getKey(), n.getSize());
 					// System.out.printf("%5d",nodes.get(j).getKey());
 				} else {
 					System.out.print("     " + "      ");
@@ -361,36 +362,35 @@ public class OrderStatisticsTree extends AbstractBinaryTree<NodeOS> {
 	public void printOrder() {
 		List<NodeOS> nodes = getListInorderWalk();
 
-        for (int j = 0; j < nodes.size(); j++) {
-            NodeOS n = nodes.get(j);
-            System.out.printf("%3d[%2d,%2d]", n.getKey(),getRank(n),getRankDesc(n));
-        }
-        System.out.println("");
+		for (int j = 0; j < nodes.size(); j++) {
+			NodeOS n = nodes.get(j);
+			System.out.printf("%3d[%2d,%2d]", n.getKey(), getRank(n),
+					getRankDesc(n));
+		}
+		System.out.println("");
 	}
 
 	public static void main(String[] args) {
-		OrderStatisticsTree tree = OrderStatisticsTree.create(
-                26, 17, 41, 14, 21,
-                30, 47, 10, 16, 19,
-                23, 28, 38, 7, 12,
-                15, 20, 35, 39, 3);
+		OrderStatisticsTree tree = OrderStatisticsTree.create(26, 17, 41, 14,
+				21, 30, 47, 10, 16, 19, 23, 28, 38, 7, 12, 15, 20, 35, 39, 3);
 		System.out.println("----------------------------------------");
 		tree.print();
 		System.out.println("----------------------------------------");
 		tree.delete(tree.search(12L));
 		tree.print();
 		System.out.println("----------------------------------------");
-        tree.printOrder();
+		tree.printOrder();
 
-        for (int i=1 ; i<=tree.size() ; i++) {
-            System.out.print(""+tree.select(tree.getRoot(),i).getKey()+" ");
-        }
-        System.out.println("");
-        for (int i=1 ; i<=tree.size() ; i++) {
-            System.out.print(""+tree.selectDesc(tree.getRoot(),i).getKey()+" ");
-        }
-        System.out.println("");
+		for (int i = 1; i <= tree.size(); i++) {
+			System.out
+					.print("" + tree.select(tree.getRoot(), i).getKey() + " ");
+		}
+		System.out.println("");
+		for (int i = 1; i <= tree.size(); i++) {
+			System.out.print("" + tree.selectDesc(tree.getRoot(), i).getKey()
+					+ " ");
+		}
+		System.out.println("");
 
-
-    }
+	}
 }
